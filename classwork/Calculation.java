d937c2ae5d7a283e584771b50deda18706f9fddf
hrantgartley <doyouevenjump@gmail.com>
2024-02-05 09:03:09 -0600

    feb5

---
+++ classwork/Calculation.java
@@ -0,0 +1,21 @@
+/**
+ * Calculation
+ */
+public class Calculation {
+    int result;
+
+    public void add(int a, int b) {
+        result = a + b;
+        System.out.println("Addition Result: " + result);
+
+    }
+
+    public void sub(int a, int b) {
+        result = a - b;
+        System.out.println("Subtraction Result: " + result);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ classwork/My_Calculation.java
@@ -0,0 +1,27 @@
+
+public class My_Calculation extends Calculation {
+    public void mul(int a, int b) {
+        result = a * b;
+        System.out.println("Multiplication Result: " + result);
+    }
+
+    public void mod(int a, int b) {
+        result = a % b;
+        System.out.println("Modulos Result: " + result);
+    }
+
+    public void div(int a, int b) {
+        result = a / b;
+        System.out.println("Division Result: " + result);
+    }
+
+    public static void main(String[] args) {
+        int val1 = 10, val2 = 20;
+        My_Calculation calc = new My_Calculation();
+        calc.add(val1, val2);
+        calc.sub(val1, val2);
+        calc.mul(val1, val2);
+        calc.mod(val1, val2);
+        calc.div(val1, val2);
+    }
+}
