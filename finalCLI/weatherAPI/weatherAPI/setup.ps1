3153c289034ed52d1a71cdb928348915b6fe15e5
hrantgartley <doyouevenjump@gmail.com>
2024-03-10 22:57:40 -0500

    fixed dirs

--- finalCLI/weatherAPI/weatherAPI/setup.ps1
+++ finalCLI/weatherAPI/weatherAPI/setup.ps1
@@ -7,7 +7,7 @@ if (-not (Test-Path -Path "./out")) {
 Remove-Item -Path "./out/production/weatherAPI/Main.class" -ErrorAction SilentlyContinue
 
 # Compile Java code
-javac -cp "./lib/json-simple-1.1.1.jar" -d "./out/production/weatherAPI" "./src/Main.java"
+javac -cp "../../lib/json-simple-1.1.1.jar" -d "./out/production/weatherAPI" "./src/Main.java"
 
 # Run Java program
-java -cp "./lib/json-simple-1.1.1.jar;./out/production/weatherAPI" Main
+java -cp "../../lib/json-simple-1.1.1.jar;./out/production/weatherAPI" Main
