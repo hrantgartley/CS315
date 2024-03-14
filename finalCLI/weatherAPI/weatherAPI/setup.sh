35c7ab576e0fed6833f511da9114d529bb83e2b6
hrantgartley <doyouevenjump@gmail.com>
2024-03-13 19:42:09 -0500

    EvenOdd Lab

--- finalCLI/weatherAPI/weatherAPI/setup.sh
+++ finalCLI/weatherAPI/weatherAPI/setup.sh
@@ -1,11 +1,7 @@
-# Create directory if it doesn't exist
-mkdir -p out
+rm -f "./out/production/weatherAPI/Main.class"
 
-# Remove Main.class file if it exists
-rm -f ./out/production/weatherAPI/Main.class
-
-# Compile Main.java with json-simple library in classpath
+# Compile Java code
 javac -cp "../../lib/json-simple-1.1.1.jar" -d "./out/production/weatherAPI" "./src/Main.java"
 
-# Execute Main class with json-simple library in classpath
-java -cp "../../lib/json-simple-1.1.1.jar;./out/production/weatherAPI" Main
+# Run Java program
+java -cp "../../lib/json-simple-1.1.1.jar:./out/production/weatherAPI" Main
